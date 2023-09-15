d50bc4ae0e0f11bd334669abd23b785675557d0a
gokhanergen-tech <ergen5171@gmail.com>
2023-09-16 02:22:28 +0300

    Updated storybook

--- example/.storybook/main.js
+++ example/.storybook/main.js
@@ -2,6 +2,7 @@ module.exports = {
   "stories": [
     "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"
   ],
+
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
@@ -9,8 +10,24 @@ module.exports = {
     "@storybook/preset-create-react-app"
     
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
+
+  "framework": {
+    name: "@storybook/react-webpack5",
+    options: {}
+  },
+
+  docs: {
+    autodocs: true
+  },
+  webpackFinal: config => {
+    return {
+      ...config,
+      plugins: config.plugins.filter(plugin => {
+        if (plugin.constructor.name === 'ESLintWebpackPlugin') {
+          return false
+        }
+        return true
+      }),
+    }
   }
 }
